49e157860934f50b7d23d78a4f643a91261803af
akhant448 <103346274+akhant448@users.noreply.github.com>
2022-10-29 18:32:02 -0700

    update for javascript

---
+++ JavaScript/Predict_Destructuring/index.js
@@ -0,0 +1,16 @@
+// const cars = ['Tesla', 'Mercedes', 'Honda']
+// const [ randomCar ] = cars
+// const [ ,otherRandomCar ] = cars
+// //Predict the output
+// console.log(randomCar)
+// console.log(otherRandomCar)
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
---
+++ JavaScript/React/functional_components/src/components/Person.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Person = (props) => {
+    return (
+        <h2>I am a person!</h2>
+    )
+}
+
+export default Person;
