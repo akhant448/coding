abf008f523814d540678905f8f9096a10cc1da5c
akhant448 <103346274+akhant448@users.noreply.github.com>
2022-11-05 09:13:40 -0700

    update

--- JavaScript/React/more-forms/src/components/Form.js
+++ JavaScript/React/more-forms/src/components/Form.js
@@ -7,6 +7,7 @@ const Form = (props) => {
     const [ password, setPassword ] = useState("");
     const [ confirmPassword, setConfirmPassword] = useState("");
 
+
     const createUser = (e) => {
         e.preventDefault();
         const newUser = {
@@ -16,49 +17,45 @@ const Form = (props) => {
             password : password,
             confirmPassword : confirmPassword
         };
-    }
+        console.log("Welcome", newUser);
+    };
     return(
         <div>
             <form onSubmit={ createUser }>
                 <div>
                     <label>First Name: </label> 
-                    <input type="text" onChange={ (e) => setFirstName(e.target.value) } />
+                    <input type="text" onChange={ (e) => setFirstName(e.target.value)} />
                 </div>
+                {firstName.length < 2 && firstName.length > 0 ? (
+                <p>First Name must be at least 2 characters</p>
+                ) : null}
                 <div>
                     <label>Last Name: </label> 
                     <input type="text" onChange={ (e) => setLastName(e.target.value) } />
                 </div>
+                {lastName.length < 2 && lastName.length > 0 ? (
+                <p>Last Name must be at least 2 characters</p>
+                ) : null}
                 <div>
                     <label>Email: </label> 
                     <input type="text" onChange={ (e) => setEmail(e.target.value) } />
                 </div>
+                {email.length < 5 && email.length > 0  ? (
+                <p>Email must be at least 5 characters</p>
+                ) : null}
                 <div>
                     <label>Password: </label>
                     <input type="password" onChange={ (e) => setPassword(e.target.value) } />
                 </div>
+                {password.length < 8 && password.length > 0 ? (
+                <p>Password must be at least 8 characters</p>
+                ) : null}
                 <div>
                     <label>Confirm Password: </label> 
                     <input type="password" onChange={ (e) => setConfirmPassword(e.target.value) } />
                 </div>
+                {confirmPassword !== password ? (<p>Password must match</p>): null}
             </form>
-            <div>
-                <h3>Your Form Data</h3>
-                <p>
-                <label>First Name: </label>{ firstName }
-                </p>
-                <p>
-                <label>Last Name: </label>{ lastName }
-                </p>
-                <p>
-                <label>Email: </label>{ email }
-                </p>
-                <p>
-                <label>Password: </label>{ password }
-                </p>
-                <p>
-                <label>Confirm Password: </label>{ confirmPassword }
-                </p>
-            </div>
         </div>
     );
 
