b96b4ceb61559f53ff3747776df28975faf5cc54
akhant448 <103346274+akhant448@users.noreply.github.com>
2022-11-18 22:49:37 -0800

    axios assignment

--- JavaScript/React/axios/src/App.js
+++ JavaScript/React/axios/src/App.js
@@ -6,26 +6,23 @@ function App() {
   const [pokemon,setPokemon] = useState([]);
 
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=807')
-    .then(response => {
-      return response.json()
-    })
-    .then(response => {
-      setPokemon(response.results)
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=807')
+    .then((response) => {
+      console.log(response.data.results)
+      setPokemon(response.data.results)
     })
     .catch((err) => {
       console.log(err);
     })
   }, []);
   return (
-      <div className="App" style={{ width: "200px", margin: "auto" }}>
-      <ul>
-        {
-          pokemon.map((pokemonObj, index)=>{
-            return (<li key={index}>{pokemonObj.name}</li>)
-          })
-        }
-      </ul>
+      <div className="App">
+        <h1>Axios Pokemon API</h1>
+          {pokemon.map((poke,index) => (
+            <ul key={index} className="pointer">
+              <li>{poke.name}</li>
+            </ul>
+        ))}
     </div>
   );
 }
